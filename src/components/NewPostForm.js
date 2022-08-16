e4096c12884147ce942e0bf4926006c4f1df7e21
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-15 19:50:09 -0700

    add file with content

---
+++ src/components/NewPostForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewPostForm(props){
+  return (
+    <React.Fragment>
+      <h3>This is a form.</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewPostForm;
