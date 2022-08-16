ae0d98c2ae41894744535f75de279c62697f3399
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-15 19:50:32 -0700

    add file with content

---
+++ src/components/PostControl.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NewPostForm from './NewPostForm';
+import PostList from './PostList.js';
+
+class PostControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default PostControl;
